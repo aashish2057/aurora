e896f3daca7b3257177a68332c0a7f3662eae4b8
Aashish Sharma <aashishsharma@Aashishs-MBP.attlocal.net>
2023-02-18 15:46:20 -0600

    sigh i keep switching this is dumb

---
+++ src/config/site.ts
@@ -0,0 +1,24 @@
+import { NavItem } from "@/types/nav"
+
+interface SiteConfig {
+  name: string
+  description: string
+  mainNav: NavItem[]
+  links: {
+    github: string
+  }
+}
+
+export const siteConfig: SiteConfig = {
+  name: "Aurora",
+  description: "Financial Dashbaord",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    github: "https://github.com/aashish2057/aurora",
+  },
+}
