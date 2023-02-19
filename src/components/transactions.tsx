5d4100d04199acb0f2a7e5948cd3109148fb361f
Aashish Sharma <aashishsharma@Aashishs-MBP.attlocal.net>
2023-02-19 07:00:50 -0600

    complete transactions list component

--- src/components/transactions.tsx
+++ src/components/transactions.tsx
@@ -5,24 +5,28 @@ import { Separator } from "@/components/ui/separator"
 import { Transactions } from "@/types/transactions"
 
 export default function TransactionsScroll({ data }: Transactions) {
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
     return (
         <>
+            <h4 className="pt-5 mb-4 text-lg text-center font-bold leading-none">Transactions</h4>
             <ScrollArea className="h-96 rounded-md border border-slate-100 dark:border-slate-700">
                 <div className="p-4">
-                    <h4 className="mb-4 text-lg text-center font-bold leading-none">Transactions</h4>
-                    <div className="w-full border-b border-b-slate-200 bg-white dark:border-b-slate-700 dark:bg-slate-900"></div>
-                    <div className="pt-5">
+                    <div className="pb-5">
+                        <div className="grid grid-cols-3 text-center">
+                            <div>Date</div>
+                            <div>Merchant</div>
+                            <div>Amount</div>
+                        </div>
+                    <div className="w-full border-b-4 border-b-slate-200 bg-white dark:border-b-slate-700 dark:bg-slate-900"></div>
+                    <div className="pt-4"></div>
                         {data.items.map((purchase) => (
                         <React.Fragment>
                             <div className="text-sm" key={purchase.id}>
                                 <div>
-                                    <div className="flex space-x-5">
-                                        <div>Amount</div>
-                                        <div>Merchant</div>
-                                    </div>
-                                    <div className="flex space-x-5">
-                                        <div>{purchase.amount}</div>
-                                        <div>{purchase.merchant_name}</div>
+                                    <div className="grid grid-cols-3 text-center">
+                                        <div>{weekday[new Date(purchase.date).getDay()]} {new Date(purchase.date).getMonth() + 1}/{new Date(purchase.date).getDate()}</div>
+                                        <div className="">{purchase.merchant_name}</div>
+                                        <div className="">{purchase.amount}</div>
                                     </div>
                                 </div>
                             </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function IndexPage({ data }: Transactions) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <TransactionsScroll data={data}></TransactionsScroll>
+      <TransactionsScroll data={data} />
     </Layout>
   )
 }
