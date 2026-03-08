48a39572e79034fed4d1b2fae8a154eac67d8a92
aashish2057 <aashish2057@gmail.com>
2026-03-08 11:42:37 -0500

    feat: add venture x csv parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use serde::Deserialize;
+use std::io;
+
+#[derive(Debug, Deserialize)]
+struct VentureXFormat {
+    #[serde(rename = "Transaction Date")]
+    transaction_date: String,
+    #[serde(rename = "Posted Date")]
+    posted_date: String,
+    #[serde(rename = "Card No.")]
+    card_number: String,
+    #[serde(rename = "Description")]
+    description: String,
+    #[serde(rename = "Category")]
+    category: String,
+    #[serde(rename = "Debit")]
+    debit: Option<f32>,
+    #[serde(rename = "Credit")]
+    credit: Option<f32>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+
+    println!("VENTURE X");
+    for result in rdr.deserialize() {
+        let record: VentureXFormat = result.expect("a CSV record");
+
+        println!("{:?}", record);
+    }
 }
